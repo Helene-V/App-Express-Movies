941a7d8e3c424bca0a35e570df8cc70628302503
Ln <rainbowliketravel@gmail.com>
2022-05-04 11:43:48 +0200

    ajout des routes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,20 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/movies', (req, res) => {
+    res.send('Bientôt des films ici même');
+});
+
+// Attention à l'ordre des routes qui aura un impact. Les routes spécialisées doivent être avant les routes plus génériques.
+app.get('/movies/add', (req, res) => {
+    res.send('prochainement, un formulaire d\'ajout ici');
+});
+
+app.get('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`film numéro ${id}`);
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
